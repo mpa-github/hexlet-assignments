9018977ea9fb8f51e35f9f281f85a613f59f5ada
@hexlet/cli <support@hexlet.io>
2023-03-06 14:26:42 +0300

    submit java-web-ru/tests

---
+++ java-web-ru/tests/src/test/java/exercise/AppTest.java
@@ -0,0 +1,142 @@
+package exercise;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import io.javalin.Javalin;
+import io.ebean.DB;
+
+import exercise.domain.User;
+import exercise.domain.query.QUser;
+import io.ebean.Database;
+
+class AppTest {
+
+    private static Javalin app;
+    private static String baseUrl;
+
+    // BEGIN
+    @BeforeAll
+    static void beforeAll() {
+        app = App.getApp();
+        app.start(0);
+
+        int port = app.port();
+
+        baseUrl = "http://localhost:" + port;
+    }
+
+    @AfterAll
+    static void afterAll() {
+        app.stop();
+    }
+    // END
+
+    // Между тестами база данных очищается
+    // Благодаря этому тесты не влияют друг на друга
+    @BeforeEach
+    void beforeEach() {
+        Database db = DB.getDefault();
+        db.truncate("user");
+        User existingUser = new User("Wendell", "Legros", "a@a.com", "123456");
+        existingUser.save();
+    }
+
+    @Test
+    void testUsers() {
+
+        // Выполняем GET запрос на адрес http://localhost:port/users
+        HttpResponse<String> response = Unirest
+            .get(baseUrl + "/users")
+            .asString();
+        // Получаем тело ответа
+        String content = response.getBody();
+
+        // Проверяем код ответа
+        assertThat(response.getStatus()).isEqualTo(200);
+        // Проверяем, что страница содержит определенный текст
+        assertThat(response.getBody()).contains("Wendell Legros");
+    }
+
+    @Test
+    void testNewUser() {
+
+        HttpResponse<String> response = Unirest
+            .get(baseUrl + "/users/new")
+            .asString();
+
+        assertThat(response.getStatus()).isEqualTo(200);
+    }
+
+    // BEGIN
+    @Test
+    void testCreateNewUser() {
+
+        HttpResponse<String> response = Unirest
+            .post(baseUrl + "/users")
+            .field("firstName", "MyName")
+            .field("lastName", "MyLastName")
+            .field("email", "my@email.com")
+            .field("password", "myPassword")
+            .asString();
+
+        assertThat(response.getStatus()).isEqualTo(302);
+
+        User actualUser = new QUser()
+            .firstName.equalTo("MyName")
+            .lastName.equalTo("MyLastName")
+            .email.equalTo("my@email.com")
+            .password.equalTo("myPassword")
+            .findOne();
+
+        assertThat(actualUser).isNotNull();
+
+        assertThat(actualUser.getFirstName()).isEqualTo("MyName");
+        assertThat(actualUser.getLastName()).isEqualTo("MyLastName");
+        assertThat(actualUser.getEmail()).isEqualTo("my@email.com");
+        assertThat(actualUser.getPassword()).isEqualTo("myPassword");
+    }
+
+    @Test
+    void testUserValidation() {
+        HttpResponse<String> response = Unirest
+            .post(baseUrl + "/users")
+            .field("firstName", "")
+            .field("lastName", "")
+            .field("email", "email.com")
+            .field("password", "myP")
+            .asString();
+
+        assertThat(response.getStatus()).isEqualTo(422);
+
+        User actualUser = new QUser()
+            .firstName.equalTo("")
+            .lastName.equalTo("")
+            .email.equalTo("email.com")
+            .password.equalTo("myP")
+            .findOne();
+
+        assertThat(actualUser).isNull();
+
+        String content = response.getBody();
+
+        assertThat(content).contains("name=\"firstName\" value=\"\"");
+        assertThat(content).contains("Имя не должно быть пустым");
+
+        assertThat(content).contains("name=\"lastName\" value=\"\"");
+        assertThat(content).contains("Фамилия не должна быть пустой");
+
+        assertThat(content).contains("name=\"email\" value=\"email.com\"");
+        assertThat(content).contains("Должно быть валидным email");
+
+        assertThat(content).contains("name=\"password\" value=\"myP\"");
+        assertThat(content).contains("Пароль должен содержать не менее 4 символов");
+    }
+    // END
+}
